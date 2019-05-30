8f3537acf64d7ec77a4c4539048d63b7bfb61473
manishnaga <mailtomaniya92@gmail.com>
2019-05-30 21:54:05 +0530

    second commit changes

---
+++ src/main/java/org/sample/testexcel/JavaClass.java
@@ -0,0 +1,26 @@
+package org.sample.testexcel;
+
+public class JavaClass {
+	//method creation
+	public void javacourse() {
+	System.out.println("java with selenium");
+	}
+	public void devopscourse() {
+	System.out.println("devops with docker");
+	}
+	public void seleniumTest() {
+		String name="welcome to selenium";
+		System.out.println(name);
+
+	}
+	
+	//main method creation
+	public static void main(String[] args) {
+	//object creation
+	      JavaClass c=new JavaClass();
+	      c.javacourse();
+	      c.devopscourse();
+			
+	}
+
+}
